e3c8d8f5fca2a83fb0117872035e864ef10cdfa0
Dramane Diakite <dramane.diakite11@gmail.com>
2025-06-29 19:05:15 -0400

    brand new spring boot gradle project

---
+++ src/test/java/com/ddsoftware/kafka_project/KafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddsoftware.kafka_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
