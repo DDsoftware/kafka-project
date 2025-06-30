96b7af0f2e93e7a7a253ed23331c91a29ade285d
Dramane Diakite <dramane.diakite11@gmail.com>
2025-06-29 20:01:12 -0400

    kafka producer project code

---
+++ src/main/java/com/ddsoftware/kafka_project/dto/Customer.java
@@ -0,0 +1,13 @@
+package com.ddsoftware.kafka_project.dto;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private int id;
+    private String name;
+    private String email;
+    private String contactNo;
+
+}
